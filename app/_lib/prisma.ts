890880956049f51452267c7535483c3d196f05e9
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2026-04-12 14:57:05 -0300

    feat: add sign in with google

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,23 +1,16 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
 
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient
 }
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-})
-
 let prisma: PrismaClient
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient({
-    adapter,
-  })
+  prisma = new PrismaClient()
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient({ adapter })
+    global.cachedPrisma = new PrismaClient()
   }
   prisma = global.cachedPrisma
 }
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
-};
+  prisma: PrismaClient | undefined
+}
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = db
 }
